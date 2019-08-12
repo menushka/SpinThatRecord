1529bfff11542acf5450fb9213ed2ed6dd59f601
Menushka Weeratunga <tmweeratunga@gmail.com>
2019-08-12 01:16:38 -0400

    Added github source

--- spinthatrecordpreferences/STRPRootListController.m
+++ spinthatrecordpreferences/STRPRootListController.m
@@ -4,6 +4,8 @@
 
 - (NSArray *)specifiers {
 	if (!_specifiers) {
+		[self setSourceUrl:@"https://github.com/menushka/SpinThatRecord"];
+		[self showSource:YES];
 		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
 	}
 
